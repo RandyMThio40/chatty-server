97a8349903e74c8c05fe54fad1124d3d170fa237
RandyMThio40 <37589527+RandyMThio40@users.noreply.github.com>
2022-01-28 19:03:34 -0500

    added chat page and friends page

--- server.js
+++ server.js
@@ -10,13 +10,35 @@ const io = new Server(httpServer, {
         methods: ["GET","POST"]
 }});
 
+
+
+let count = 0;
 io.on("connection", (socket) => {
     socket.emit("is-connected",true);
-    socket.on("has-connected",(data)=>{
-        console.log("data: ", data.user.uid);
-        socket.emit("response","connected bitch");
+    socket.on("join-room",(room)=>{
+        socket.join(room);
+        console.log("socket.id: ",socket.id);
     })
+    socket.on("leave-room",(room)=>{
+        console.log("left room id: ",room);
+        socket.leave(room);
+    })
+    socket.on("sent",(current_room, data)=>{
+        console.log("count: ", count++);
+        console.log("socket.id: ",socket.id, "roooms: ", socket.adapter.rooms, " current room",current_room);
 
+        socket.to(current_room).emit("receive-message",data)
+    })
+    socket.on("check-rooms",()=>{
+        console.log("socket.id: ",socket.id, "roooms: ", socket.adapter.rooms,"the room: ", socket.rooms);
+    })
+    socket.on("disconnecting", () => {
+        console.log("disconnecting",socket.rooms); // the Set contains at least the socket ID
+      });
+      socket.on("disconnect", () => {
+        // socket.rooms.size === 0
+        console.log("disconnected")
+      });
     console.log("connected: ",socket.id);
 });
 
