7985d946eb00787e3f1644774cfd83b257d21c7e
RandyMThio40 <37589527+RandyMThio40@users.noreply.github.com>
2022-04-06 16:01:11 -0400

    used deployed url

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const axios = require("axios");
 const io = new Server(httpServer,{ 
     cors:{ 
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000","https://glistening-marzipan-f601c4.netlify.app"],
         methods: ["GET","POST"],
         credentials:true,
     }
@@ -16,7 +16,7 @@ const io = new Server(httpServer,{
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000","https://glistening-marzipan-f601c4.netlify.app"],
         methods: ["GET","POST"],
         credentials:true,
     })
